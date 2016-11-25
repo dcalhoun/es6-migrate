abf09b4ee97495bdb856be69ed933389c7bb65e9
David Calhoun <dpcalhoun@gmail.com>
2016-11-25 09:27:37 -0600

    Add error resilience to CoffeeScript conversion
    Convert to asynchronous reads and writes. Add resilience to errors.

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,9 +1,17 @@
 let fs = jest.genMockFromModule('fs')
 
-fs.readFileSync = jest.fn((file) => (
-  `Text content for ${file}.`
-))
+fs.readFileSync = jest.fn((file) => (`Text content for ${file}.`))
 fs.unlinkSync = jest.fn()
 fs.writeFileSync = jest.fn()
+
+fs.readFile = jest.fn((file, cb) => {
+  cb(undefined, `Text content for ${file}.`)
+})
+
+fs.unlink = jest.fn()
+
+fs.writeFile = jest.fn((dest, content, cb) => {
+  cb()
+})
 
 module.exports = fs
