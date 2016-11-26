2fd3a86dae70974eeb9af58eeb931c53c1d54e79
David Calhoun <dpcalhoun@gmail.com>
2016-11-26 17:28:06 -0600

    Replace match CLI option
    Infer match extension based on first file passed.

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -4,7 +4,9 @@ fs.readFile = jest.fn((file, cb) => {
   cb(undefined, `Text content for ${file}.`)
 })
 
-fs.unlink = jest.fn()
+fs.unlink = jest.fn((path, cb) => {
+  cb()
+})
 
 fs.writeFile = jest.fn((dest, content, cb) => {
   cb()
