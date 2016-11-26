57c69a200954fa739f80100bf9673bd1d93bab2c
David Calhoun <dpcalhoun@gmail.com>
2016-11-26 08:14:32 -0600

    Convert `clean` and `convertModules` to async

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,9 +1,5 @@
 let fs = jest.genMockFromModule('fs')
 
-fs.readFileSync = jest.fn((file) => (`Text content for ${file}.`))
-fs.unlinkSync = jest.fn()
-fs.writeFileSync = jest.fn()
-
 fs.readFile = jest.fn((file, cb) => {
   cb(undefined, `Text content for ${file}.`)
 })
